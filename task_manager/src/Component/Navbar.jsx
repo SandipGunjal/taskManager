393d9d8b91da73e82c77be0b09e571b5027893cc
Your Name <you@example.com>
2024-09-26 11:23:22 +0530

    change css

--- task_manager/src/Component/Navbar.jsx
+++ task_manager/src/Component/Navbar.jsx
@@ -1,45 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import '../CSS/Navbar.css'
-
+import "../CSS/Navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="">
       <nav className="navbar navbar-expand-lg navbar-light bg-primary">
-        <Link className="navbar-brand">
-          Task Manager
-        </Link>
+        <Link className="navbar-brand">Task Manager</Link>
         <button
           className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
+          onClick={toggleMenu}
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className={`navbar-nav ${isOpen ? "show" : ""}`}>
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <Link className="nav-link" to='/' >
-                Home 
+              <Link className="nav-link" to="/">
+                Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to='/login'>
+              <Link className="nav-link" to="/login">
                 Login
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to='/register'>
+              <Link className="nav-link" to="/register">
                 Register
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to='/tasks' >
+              <Link className="nav-link" to="/tasks">
                 Task List
               </Link>
             </li>
