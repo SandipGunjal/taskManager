a50a20b3b3a7994fed760a154b1de1ac96accb65
Your Name <you@example.com>
2024-09-26 12:29:37 +0530

    #

--- Backend/server.js
+++ Backend/server.js
@@ -1,24 +1,26 @@
 // backend/server.js
-const express = require('express');
-const cors = require('cors')
-const dotenv = require('dotenv');
-const connectDB = require('./db');
-const authRoutes = require('./Routes/authRoute');
-const taskRoutes = require('./Routes/taskRoutes');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./db");
+const authRoutes = require("./Routes/authRoute");
+const taskRoutes = require("./Routes/taskRoutes");
 
 dotenv.config();
 connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:3000', // Your frontend origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Authorization', 'Content-Type']
-}))
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Your frontend origin
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+  })
+);
 
-app.use('/api/auth', authRoutes);
-app.use('/api/tasks', taskRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
